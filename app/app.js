812f4c4503e90dc8bc44b25cc9f0c5f8c734e571
Dan-Hodges <dhdgs690@gmail.com>
2015-09-17 15:43:06 -0500

    added functions to manipulate data

--- app/app.js
+++ app/app.js
@@ -1,23 +1,38 @@
-angular.module("materialExample", ["ngMaterial", "materialCalendar", "firebase"]);
+angular.module("materialExample", ["ngMaterial", "materialCalendar", "firebase"])
+.constant('_', window._);
 
 angular.module("materialExample").controller("calendarCtrl", function($scope, $firebaseObject, $filter) {
-  var ref = new Firebase("https://soccerdates.firebaseio.com/");
-  $scope.data = $firebaseObject(ref);
 
-$scope.data.$loaded()
-  .then(function() {
-    $scope.games = $scope.data.TCA.schedule;
-    console.log("TCA Games :", $scope.games);
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
+  $scope._ = _;
+  var ref = new Firebase("https://soccerdates.firebaseio.com/");; // assume value here is { foo: "bar" }
+  var fireCal = $firebaseObject(ref);
+  // $scope.calendar.weeksObj = {};
+  var allDemWeeks = {};
 
-
+  if ($scope.calendar) {
+    var weekCounter = 0;
+    var dayCounter = 0;
+    for (key in $scope.calendar.weeks) {
+      allDemWeeks[weekCounter] = $scope.calendar.weeks[key].slice();
+      weekCounter ++;
+      // console.log($scope.calendar.weeks[key]);
+      for (key2 in $scope.calendar.weeks[key]){
+        allDemWeeks[key][key2] = {
+          date: $scope.calendar.weeks[key][key2]
+        } 
+        console.log($scope.calendar.weeks[key][key2]);
+        dayCounter++;
+      }
+    }
+    console.log("allDemWeeks :", allDemWeeks);
+    $scope.calendar.allDemWeeks = allDemWeeks;
+  }
+  
   $scope.selectedDate = null;
   $scope.setDirection = function(direction) {
     $scope.direction = direction;
   };
+  // $scope.setDirection("verticle");
   $scope.selectedDate;
   $scope.dayClick = function(date) {
     $scope.msg = "You clicked " + $filter("date")(date, "MMM d, y h:mm:ss a Z");
@@ -28,10 +43,9 @@ $scope.data.$loaded()
   $scope.nextMonth = function(data) {
     $scope.msg = "You clicked (next) month " + data.month + ", " + data.year;
   };
-  $scope.setDayContent = function() {
+  // $scope.setDayContent = function() {
     
-    return ;
-  };
+  //   return $scope.calendar;
+  // };
 
-  console.log("$scope.calendar in app :", $scope.calendar);
 });
