e945a242d99bc24ec6168632095e20322743a933
Dan-Hodges <dhdgs690@gmail.com>
2015-09-18 15:35:09 -0500

    saving data to firebase

--- app/app.js
+++ app/app.js
@@ -4,54 +4,62 @@ angular.module("materialExample", ["ngMaterial", "materialCalendar", "firebase"]
 angular.module("materialExample").controller("calendarCtrl", function($scope, $firebaseObject, $filter) {
 
   $scope._ = _;
+  var ref = new Firebase("https://soccerdates.firebaseio.com/"); // assume value here is { foo: "bar" }
+  var fireCal = $firebaseObject(ref);
+  // console.log("fireCal :", fireCal);  
 
-  $scope.onEditChange = function () {
-    ///$scope.calendar is the object created by Matertial Calendar///
-    ///I am modifying it into something I can work with (add games, etc...)///
-    var allWeeks = {};
-    if ($scope.calendar) {
-      var weekCounter = 0;
-      var dayCounter = 0;
-      ///looping over weeks array-ish objects///
-      for (key in $scope.calendar.weeks) {
-        allWeeks[weekCounter] = $scope.calendar.weeks[key].slice();
-        weekCounter ++;
-        ///looping over days array-ish objects///
-        for (key2 in $scope.calendar.weeks[key]){
-          allWeeks[key][key2] = {
-            ///creating a JSON friendly key value pair I can use to sync firebase data-object -->
-            /// with local $scope.calendar.weeks/allweeks object///
-            id: angular.toJson($scope.calendar.weeks[key][key2]),
-            date: $scope.calendar.weeks[key][key2],
-            games: {first:""}
-          }
-          dayCounter++;
+  if ($scope.calendar) {
+    console.log("$scope.calendar :", $scope.calendar);
+    $scope.calendar.localCal = {};
+  }
+
+  $scope.ngObjFixHack = function(ngObj) {
+    var output;
+    output = angular.toJson(ngObj);
+    output = angular.fromJson(output);
+    return output;
+  }  
+
+  ///$scope.calendar is the object created by Matertial Calendar///
+  ///I am modifying it into something I can work with (add games, etc...)///
+  var allWeeks = {};
+  if ($scope.calendar) {
+    var weekCounter = 0;
+    var dayCounter = 0;
+    ///looping over weeks array-ish objects///
+    for (key in $scope.calendar.weeks) {
+      allWeeks[weekCounter] = $scope.calendar.weeks[key].slice();
+      weekCounter ++;
+      ///looping over days array-ish objects///
+      for (key2 in $scope.calendar.weeks[key]){
+        allWeeks[key][key2] = {
+          ///creating a JSON friendly key value pair I can use to sync firebase data-object -->
+          /// with local $scope.calendar.weeks/allweeks object///
+          id: angular.toJson($scope.calendar.weeks[key][key2]),
+          date: $scope.calendar.weeks[key][key2],
+          games: {first:""}
         }
+        dayCounter++;
       }
-      $scope.calendar.weeks = allWeeks;
-      // console.log("allWeeks :", allWeeks)
-      // console.log("$scope.calendar.weeks :", $scope.calendar.weeks);
     }
+    $scope.calendar.weeks = allWeeks;
+    // console.log("allWeeks :", allWeeks)
+    console.log("$scope.calendar.weeks :", $scope.calendar.weeks);
+  }
 
-    //removes $$hashkey from objects to make them json/firebase friendly
-    $scope.ngObjFixHack = function(ngObj) {
-      var output;
-      output = angular.toJson(ngObj);
-      output = angular.fromJson(output);
-      return output;
-    }  
+  //removes $$hashkey from objects to make them json/firebase friendly
 
-    var localCal = {};
+  function updateLocal() {
     if ($scope.calendar) {
       var weekCounter = 0;
       var dayCounter = 0;
       ///looping over weeks array-ish objects///
       for (key in $scope.calendar.weeks) {
-        localCal[weekCounter] = $scope.calendar.weeks[key].slice();
+        $scope.calendar.localCal[weekCounter] = $scope.calendar.weeks[key].slice();
         weekCounter ++;
         ///looping over days array-ish objects///
         for (key2 in $scope.calendar.weeks[key]){
-          localCal[key][key2] = {
+          $scope.calendar.localCal[key][key2] = {
             clicked: $scope.calendar.weeks[key][key2].clicked,
             id: $scope.calendar.weeks[key][key2].id,
             games: $scope.calendar.weeks[key][key2].games
@@ -59,37 +67,45 @@ angular.module("materialExample").controller("calendarCtrl", function($scope, $f
           dayCounter++;
         }
       }
-      localCal = $scope.ngObjFixHack(localCal);
-      console.log("hacked localCal :", localCal);
+      $scope.calendar.localCal = $scope.ngObjFixHack($scope.calendar.localCal);
     }
-    
-    var ref = new Firebase("https://soccerdates.firebaseio.com/"); // assume value here is { foo: "bar" }
-    var fireCal = $firebaseObject(ref);
-
-    console.log("fireCal :", fireCal);
-
-    if ($scope.calendar && fireCal) {
-      var weekCounter = 0;
-      var dayCounter = 0;
-      ///looping over weeks array-ish objects///
-      for (key in localCal) {
-        fireCal[weekCounter] = localCal[weekCounter];
-        weekCounter ++;
-        ///looping over days array-ish objects///
-        for (key2 in localCal[key]){
-          fireCal[key][key2] = {
-            id: localCal[key][key2].id,
-            games: localCal[key][key2].games
-          }
-          dayCounter++;
+  }
+  
+  function updateFire () {
+    var weekCounter = 0;
+    var dayCounter = 0;
+    ///looping over weeks array-ish objects///
+    for (key in $scope.calendar.localCal) {
+      fireCal[weekCounter] = $scope.calendar.localCal[weekCounter];
+      weekCounter ++;
+      ///looping over days array-ish objects///
+      for (key2 in $scope.calendar.localCal[key]){
+        fireCal[key][key2] = {
+          id: $scope.calendar.localCal[key][key2].id,
+          games: $scope.calendar.localCal[key][key2].games
         }
+        if ($scope.calendar.weeks[key][key2].clicked) {
+          fireCal[key][key2].clicked = $scope.calendar.weeks[key][key2].clicked;
+          console.log("got it");
+        }
+        dayCounter++;
       }
-      fireCal.$save().then(function(ref) {
-        ref.key() === fireCal.$id; // true
-      }, function(error) {
-        console.log("Error:", error);
-      });    
     }
+    fireCal.$save().then(function(ref) {
+      ref.key() === fireCal.$id; // true
+    }, function(error) {
+      console.log("Error:", error);
+    });    
+  }
+
+  $scope.onUpdate = function() {
+    updateLocal();
+    updateFire();
+    console.log("onUpdate");
+  };
+
+  if($scope.calendar) {
+    $scope.onUpdate();
   }
 
   $scope.selectedDate = null;
@@ -111,6 +127,4 @@ angular.module("materialExample").controller("calendarCtrl", function($scope, $f
     
   //   return $scope.calendar;
   // };
-  $scope.onEditChange();
-
 });
