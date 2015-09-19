cbb0f038ea1acbea51bd65f18061521ec3386917
Dan-Hodges <dhdgs690@gmail.com>
2015-09-19 16:06:19 -0500

    did some swell coding

--- app/app.js
+++ app/app.js
@@ -4,15 +4,9 @@ angular.module("materialExample", ["ngMaterial", "materialCalendar", "firebase"]
 angular.module("materialExample").controller("calendarCtrl", function($scope, $firebaseObject, $filter) {
 
   $scope._ = _;
-  var ref = new Firebase("https://soccerdates.firebaseio.com/"); // assume value here is { foo: "bar" }
+  var ref = new Firebase("https://soccerdates.firebaseio.com/");
   var fireCal = $firebaseObject(ref);
-  // console.log("fireCal :", fireCal);  
-  if ($scope.calendar) {
-    console.log("$scope.calendar :", $scope.calendar);
-    $scope.calendar.localCal = {};
-  }
 
-  //removes $$hashkey from objects to make them json/firebase friendly
   $scope.ngObjFixHack = function(ngObj) {
     var output;
     output = angular.toJson(ngObj);
@@ -20,139 +14,95 @@ angular.module("materialExample").controller("calendarCtrl", function($scope, $f
     return output;
   }  
 
-  ///$scope.calendar is the object created by Matertial Calendar///
-  ///I am modifying it into something I can work with (add games, etc...)///
-  var allWeeks = {};
-  var months = [];
-  if ($scope.calendar) {
-    var weekCounter = 0;
-    var dayCounter = 0;
-    ///looping over weeks array-ish objects///
-    for (key in $scope.calendar.weeks) {
-      // console.log("$scope.calendar.localCal first:", $scope.calendar.localCal);
-      allWeeks[weekCounter] = $scope.calendar.weeks[key].slice();
-      // console.log('$scope.calendar.weeks in beforehack :', $scope.calendar.weeks);
-      weekCounter ++;
-      ///looping over days array-ish objects///
-      for (key2 in $scope.calendar.weeks[key]){
-        allWeeks[key][key2] = {
-          ///creating a JSON friendly key value pair I can use to sync firebase data-object -->
-          /// with local $scope.calendar.weeks/allweeks object///
-          id: angular.toJson($scope.calendar.weeks[key][key2]),
-          date: $scope.calendar.weeks[key][key2],
-          games: {first:""}
-        }
-        dayCounter++;
-      }
-    }
-    console.log($scope.calendar.month);
-    $scope.calendar.weeks = allWeeks;
-    // console.log("allWeeks :", allWeeks);
-    // console.log("$scope.calendar.weeks after allweeks :", $scope.calendar.weeks);
-  }
 
-  function updateLocal() {
-    if ($scope.calendar) {
-      var weekCounter = 0;
-      var dayCounter = 0;
-      ///looping over weeks array-ish objects///
-      for (key in $scope.calendar.weeks) {
-        // console.log('$scope.calendar.weeks in updateLocal :', $scope.calendar.weeks);
-        $scope.calendar.localCal[weekCounter] = $scope.calendar.weeks[key].slice();
-        weekCounter ++;
-        ///looping over days array-ish objects///
-        for (key2 in $scope.calendar.weeks[key]){
-          $scope.calendar.localCal[key][key2] = {
-            clicked: $scope.calendar.weeks[key][key2].clicked,
-            id: $scope.calendar.weeks[key][key2].id,
-            games: $scope.calendar.weeks[key][key2].games
-          }
-          dayCounter++;
-        }
-      }
-      $scope.calendar.localCal = $scope.ngObjFixHack($scope.calendar.localCal);
-    }
-  }
+
+  // function updateLocal() {
+  //     var weekCounter = 0;
+  //     var dayCounter = 0;
+  //     for (key in $scope.calendar.weeks) {
+  //       $scope.calendar.localCal[weekCounter] = $scope.calendar.weeks[key].slice();
+  //       weekCounter ++;
+  //       for (key2 in $scope.calendar.weeks[key]){
+  //         $scope.calendar.localCal[key][key2] = {
+  //           clicked: $scope.calendar.weeks[key][key2].clicked,
+  //           id: $scope.calendar.weeks[key][key2].id,
+  //           games: $scope.calendar.weeks[key][key2].games
+  //         }
+  //         dayCounter++;
+  //       }
+  //     }
+  // }
+  // if ($scope.calendar) {
+    // $scope.localCal=localCal = $scope.ngObjFixHack($scope.calendar.weeks);
+  //   console.log(localCal);
+  // }
   
-  function updateFire () {
-    var weekCounter = 0;
-    var dayCounter = 0;
-    ///looping over weeks array-ish objects///
-    for (key in $scope.calendar.localCal) {
-      fireCal[weekCounter] = $scope.calendar.localCal[weekCounter];
-      weekCounter ++;
-      ///looping over days array-ish objects///
-      for (key2 in $scope.calendar.localCal[key]){
-        fireCal[key][key2] = {
-          id: $scope.calendar.localCal[key][key2].id,
-          games: $scope.calendar.localCal[key][key2].games
-        }
-        if ($scope.calendar.weeks[key][key2].clicked) {
-          fireCal[key][key2].clicked = $scope.calendar.weeks[key][key2].clicked;
-          console.log("got it");
-        }
-        dayCounter++;
-      }
-    }
-    console.log("fireCal :", fireCal);
-    // var myObject = _.merge($scope.calendar.weeks, fireCal);
-    // console.log("myObject :", myObject);
-    fireCal.$save().then(function(ref) {
-      ref.key() === fireCal.$id; // true
-    }, function(error) {
-      console.log("Error:", error);
-    });    
-  }
+  // function updateFire () {
+  //   var weekCounter = 0;
+  //   var dayCounter = 0;
+  //   for (key in $scope.calendar.localCal) {
+  //     fireCal[weekCounter] = $scope.calendar.localCal[weekCounter];
+  //     weekCounter ++;
+  //     for (key2 in $scope.calendar.localCal[key]){
+  //       fireCal[key][key2] = {
+  //         id: $scope.calendar.localCal[key][key2].id,
+  //         games: $scope.calendar.localCal[key][key2].games
+  //       }
+  //       if ($scope.calendar.weeks[key][key2].clicked) {
+  //         fireCal[key][key2].clicked = $scope.calendar.weeks[key][key2].clicked;
+  //         console.log("got it");
+  //       }
+  //       dayCounter++;
+  //     }
+  //   }
+  //   console.log("fireCal :", fireCal);
+  //   fireCal.$save().then(function(ref) {
+  //     ref.key() === fireCal.$id; // true
+  //   }, function(error) {
+  //     console.log("Error:", error);
+  //   });    
+  // }
 
-  //next step is to dynamically merge fireCal with $scope.calendar.weeks
+  // function bringTheFire () {
+  //   fireCal.$loaded()
+  //   .then(function() {
+  //     for (key in $scope.calendar.weeks) {
+  //       for (key2 in $scope.calendar.weeks[key]) {
+  //         if($scope.calendar.weeks[key][key2].id) {
+  //           var dateId = $scope.calendar.weeks[key][key2].id;
+  //           for (key3 in fireCal) {
+  //             for (key4 in fireCal[key3]) {              
+  //               if (fireCal[key3][key4].id === dateId) {
+  //                 if(fireCal[key3][key4].clicked){
+  //                   $scope.calendar.weeks[key][key2].clicked = fireCal[key3][key4].clicked;
+  //                 };
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   })
+  // }
 
-  function bringTheFire () {
-    fireCal.$loaded()
-    .then(function() {
-      console.log($scope.calendar.weeks);
-      console.log(fireCal);
-      for (key in $scope.calendar.weeks) {
-        for (key2 in $scope.calendar.weeks[key]) {
-          // console.log("for check");
-          if($scope.calendar.weeks[key][key2].id) {
-            // console.log("if check");
-            var dateId = $scope.calendar.weeks[key][key2].id;
-            // console.log(dateId);
-            for (key3 in fireCal) {
-              for (key4 in fireCal[key3]) {              
-                if (fireCal[key3][key4].id === dateId) {
-                  if(fireCal[key3][key4].clicked){
-                    $scope.calendar.weeks[key][key2].clicked = fireCal[key3][key4].clicked;
-                    console.log($scope.calendar.weeks[key][key2]);
-                  };
-                }
-              }
-            }
-          }
-        }
-      }
-    })
-  }
-
-
-  $scope.onUpdate = function() {
-    updateLocal();
-    updateFire();
-  };
+  // $scope.onUpdate = function() {  
+  //   firstStep();   
+  //   // updateLocal();
+  //   // updateFire();
+  // };
   $scope.onLoad = function() {
-    updateLocal();
-    bringTheFire();
+    // firstStep();
+    // updateLocal();
+    // bringTheFire();
   }
 
   if($scope.calendar) {
     $scope.onLoad();
+    console.log($scope.calendar);
   }
-
-
-
-
-
   $scope.selectedDate = null;
+
+
   $scope.setDirection = function(direction) {
     $scope.direction = direction;
   };
@@ -172,3 +122,21 @@ angular.module("materialExample").controller("calendarCtrl", function($scope, $f
   //   return $scope.calendar;
   // };
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
