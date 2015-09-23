b7acd0d56f164afcb9c92e307fccca5a19e70ff4
Dan-Hodges <dhdgs690@gmail.com>
2015-09-23 10:18:56 -0500

    functional calendar stores and retrieves data

--- app/app.js
+++ app/app.js
@@ -1,127 +1,92 @@
 angular.module("materialExample", ["ngMaterial", "materialCalendar", "firebase"])
 .constant('_', window._);
 
-angular.module("materialExample").controller("calendarCtrl", function($scope, $firebaseObject, $filter) {
+angular.module("materialExample").controller("calendarCtrl", function($scope, $firebaseObject, $firebaseArray, $filter) {
 
   $scope._ = _;
 
-
-
-  if($scope.calendar) {
-
-  }
-
-
-
-
-
-
-  // FB.$bindTo($scope, "calendar.fireCal").then(function(unbind) {
-  //   console.log()
-  //   //unbind();
-  // });
-
-
-
-
-
-  $scope.onLoad = function() {
-    // firstStep();
-    // updateLocal();
-    // bringTheFire();
+  if ($scope.calendar) {
+    console.log($scope.calendar);
   }
 
   $scope.selectedDate = null;
-
-
   $scope.setDirection = function(direction) {
     $scope.direction = direction;
   };
   // $scope.setDirection("verticle");
   $scope.selectedDate;
+
   $scope.dayClick = function(date) {
     $scope.msg = "You clicked " + $filter("date")(date, "MMM d, y h:mm:ss a Z");
-    console.log(date);
+    // setTimeout(function(){
+    //   $scope.weeksToDates();
+    // }(),3000);
+    // setTimeout(function(){
+    //   $scope.datesUpToFire();
+    // }(),3000);
   };
-  // $scope.prevMonth = function(data) {
-  //   $scope.msg = "You clicked (prev) month " + data.month + ", " + data.year;
-  //     if($scope.calendar) {
-  //   FB[$scope.calendar.month] = $scope.calendar.fireCal;
-  //   FB.$save().then(function(ref) {
-  //     ref.key() === FB.$id; // true
-  //   }, function(error) {
-  //     console.log("Error:", error);
-  //   });
-  // }
-  // };
-  // $scope.nextMonth = function(data) {
-  //   $scope.msg = "You clicked (next) month " + data.month + ", " + data.year;
-  //   if ($scope.calendar) {
-  //     if (data.month === 9) {    
-  //       var ref = new Firebase("https://soccerdates.firebaseio.com/");
-  //       var FB = $firebaseObject(ref);
-  //       FB[9] = $scope.calendar.fireCal;
-  //       FB.$save().then(function(ref) {
-  //         ref.key() === FB.$id; // true
-  //       }, function(error) {
-  //         console.log("Error:", error);
-  //       });
-  //     }    
-  //   }
-  // };
 
+  $scope.nextMonth = function(data) {
+    $scope.msg = "You clicked (next) month " + data.month + ", " + data.year;
+    // var timoutId =;
+    // function delayedCode() {
+    //   timoutId = window.setTimeout(runCode, 2000);
+    // }
+    // function runCode () {
+    //   weeksToDates();
+    //   datesUpToFire();
+    // }
+  }    
 });
-
-
-
-  // $scope.setDayContent = function() {
-    
-  //   return $scope.calendar;
-  // };
-
-
-  //   fireCal.$save().then(function(ref) {
-  //     ref.key() === fireCal.$id; // true
-  //   }, function(error) {
-  //     console.log("Error:", error);
-  //   });    
-  // }
-
-  // function bringTheFire () {
-  //   $scope.calendar.fireCal[$scope.calendar.month].$loaded()
+  // function downFromFire () {
+  //   var ref = new Firebase("https://soccerdates.firebaseio.com");
+  //   $scope.FB = $firebaseObject(ref);
+  //   $scope.FB.$loaded()
   //   .then(function() {
-  //     for (key in $scope.calendar.weeks[$scope.calendar.month] {
-  //       for (key2 in $scope.calendar.weeks[$scope.calendar.month[key]) {
-  //         if($scope.calendar.weeks[$scope.calendar.month[key][key2].date) {
-  //           var dateId = $scope.calendar.weeks[$scope.calendar.month][key][key2].date;
-  //           for (key3 in $scope.calendar.fireCal[$scope.calendar.month]) {
-  //             for (key4 in $scope.calendar.fireCal[$scope.calendar.month][key3]) {              
-  //               if ($scope.calendar.fireCal[$scope.calendar.month][key3][key4].date === dateId) {
-  //                 $scope.calendar.weeks[$scope.calendar.month[key][key2].clicked = $scope.calendar.fireCal[$scope.calendar.month][key3][key4].clicked;
-  //               }  
-  //             }
+  //     console.log("downFromFire");
+  //     for (key in $scope.calendar.weeks[$scope.calendar.month]) {
+  //       for (key2 in $scope.calendar.weeks[$scope.calendar.month][key]) {
+  //         var date = $scope.calendar.weeks[$scope.calendar.month][key][key2].jsonId;
+  //         // console.log($scope.calendar.month);
+  //         var index = $scope.calendar.month;
+  //         for (i in $scope.FB[index]){            
+  //           if ($scope.FB[$scope.calendar.month][i] === date) {
+  //             $scope.calendar.weeks[$scope.calendar.month][key][key2].info = angular.copy($scope.FB[$scope.calendar.month][i].info);
+  //             console.log("$scope.FB[$scope.calendar.month][i].info :", $scope.FB[$scope.calendar.month][i].info)
+  //             console.log("match");
   //           }
   //         }
   //       }
-  //     }
+  //     } 
   //   })
+  //   .catch(function(err) {
+  //     console.error(err);
+  //   });
+  // };
+  // if ($scope.calendar) {
+  //   downFromFire();
   // }
 
-  // $scope.onUpdate = function() {  
-  //   firstStep();   
-  //   // updateLocal();
-  //   // updateFire();
+  // function weeksToDates (){
+  //   for (key in $scope.calendar.weeks[$scope.calendar.month]) {
+  //     for (key2 in $scope.calendar.weeks[$scope.calendar.month][key]) {
+  //       var date = $scope.calendar.weeks[$scope.calendar.month][key][key2].jsonId;
+  //       console.log(date);
+  //       for (var i in $scope.calendar.dates[$scope.calendar.month]){            
+  //         if ($scope.calendar.dates[$scope.calendar.month][i] === date) {
+  //           $scope.calendar.weeks[$scope.calendar.month][key][key2].info = angular.copy($scope.calendar.dates[$scope.calendar.month][i].info);
+  //         }
+  //       }
+  //     }
+  //   } 
   // };
 
-
-
-
-
-
-
-
-
-
-
-
-
+  // function datesUpToFire () {
+  //   console.log("$scope.calendar.date[$scope.calendar.month] :", $scope.calendar.dates[$scope.calendar.month]);
+  //   var happy = $scope.calendar.dates[$scope.calendar.month];
+  //   var number = $scope.calendar.month;
+  //   var ref = new Firebase("https://soccerdates.firebaseio.com/" + number);
+  //   ref.update(number=happy);
+  //   console.log("happy :", happy);
+  //   console.log("number :", number);
+  // };
