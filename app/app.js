7049ec27c8543f95bfafc99e9603f48eeeeb6431
Dan-Hodges <dhdgs690@gmail.com>
2015-09-24 09:33:37 -0500

    updated with drawer and dialog

--- app/app.js
+++ app/app.js
@@ -1,12 +1,59 @@
 angular.module("materialExample", ["ngMaterial", "materialCalendar", "firebase"])
 .constant('_', window._);
 
-angular.module("materialExample").controller("calendarCtrl", function($scope, $firebaseObject, $firebaseArray, $filter) {
+angular.module("materialExample").controller("calendarCtrl", function($scope, $firebaseObject, $firebaseArray, $filter, $mdDialog, $timeout, $mdSidenav, $log, $mdUtil) {
 
   $scope._ = _;
 
   if ($scope.calendar) {
     console.log($scope.calendar);
+  }
+
+  $scope.close = function () {
+    $mdSidenav('right').close()
+      .then(function () {
+        $log.debug("close RIGHT is done");
+      });
+  }
+
+  $scope.toggleRight = buildToggler('right');
+  function buildToggler(navID) {
+    var debounceFn =  $mdUtil.debounce(function(){
+      $mdSidenav(navID)
+        .toggle()
+        .then(function () {
+          $log.debug("toggle " + navID + " is done");
+        });
+      },200);
+    return debounceFn;
+  }
+  $scope.toggleRight();
+
+
+  $scope.showAdvanced = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'dialog1.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+  function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
   }
 
   $scope.selectedDate = null;
