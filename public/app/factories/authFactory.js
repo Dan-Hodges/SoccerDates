edb8a5c6c71cc7f4dd6116f6943918cbc9bbd580
DanHodges <dhdgs690@gmail.com>
2015-12-02 13:57:28 -0600

    updated dialogs

--- public/app/factories/authFactory.js
+++ public/app/factories/authFactory.js
@@ -1,7 +1,6 @@
 app.factory("Auth", ["$firebaseAuth",
   function($firebaseAuth) {
     var ref = new Firebase("https://soccerdates.firebaseio.com");
-    alert("To view demo this app, login with TCA@TCA.com, pass");
     return $firebaseAuth(ref);
   }
 ]);
